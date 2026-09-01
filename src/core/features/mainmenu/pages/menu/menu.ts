394d04af353151a4a6328bf1ee7541c8b5156294
qu4x <mdjanas@gmail.com>
2026-09-01 19:38:56 +0200

    fix(ui): fix getTabIcon method declaration in menu.ts and verify clean build

--- src/core/features/mainmenu/pages/menu/menu.ts
+++ src/core/features/mainmenu/pages/menu/menu.ts
@@ -389,6 +389,7 @@ export default class CoreMainMenuPage implements OnInit, OnDestroy {
      * @param fallbackIcon Optional fallback icon from handler.
      * @returns Icon name.
      */
+    getTabIcon(page: string, isSelected: boolean, fallbackIcon?: string): string {
         switch (page) {
             case 'site':
             case 'sitehome':
